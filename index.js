30f7f3d2c688a09ea5b76680e6b32f367a529542
Rimvydas Zilinskas <rimvydas.zilinskas@yahoo.com>
2020-03-30 20:58:45 +0200

    extract raw path

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use((req, res, next) => {
+  let path = console.log(req._parsedUrl.pathname);
+
+  if (path === "/") {
+    // reset model here
+  }
+
+  next();
+});
+
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 app.listen(3000, err => {
   if (err) {
