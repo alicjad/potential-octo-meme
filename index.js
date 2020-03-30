a10fc16c75bc78b5143934b8c6bcdb1471fedeeb
Rimvydas Zilinskas <rimvydas.zilinskas@yahoo.com>
2020-03-30 18:49:07 +0200

    express init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, err => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+
+  console.log("Listening on port 3000");
+});
